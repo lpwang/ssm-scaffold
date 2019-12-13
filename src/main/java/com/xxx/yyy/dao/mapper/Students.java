3329ca0a8d3ee80dd14b0bdc10e0264e913e8bf6
lpwang <13889522960@163.com>
2019-12-13 23:16:23 +0800

    添加日志

--- src/main/java/com/xxx/yyy/dao/mapper/Students.java
+++ src/main/java/com/xxx/yyy/dao/mapper/Students.java
@@ -1,7 +1,10 @@
 package com.xxx.yyy.dao.mapper;
 
 import com.xxx.yyy.dao.entry.StudentEntry;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -9,5 +12,14 @@ public interface Students {
 
     @Select("select * from students")
     List<StudentEntry> getAllStudents();
+
+    @Insert("insert into students (name,age) values (#{student.name},#{student.age})")
+    int addStudent(StudentEntry student);
+
+    @Update("update students set name=#{student.name},age=#{student.age} where id=#{student.id}")
+    int updateStudent(StudentEntry student);
+
+    @Delete("delete from students where id=#{param1}")
+    int deleteStudent(int id);
 
 }
